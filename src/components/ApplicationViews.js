3b8a88bd001ff8ed1be56406a046f993208b3252
Gavin Schriver <alexanderschriver@gmail.com>
2020-11-04 11:36:45 -0600

    initial app setup

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route } from "react-router-dom";
+// import { GameProvider } from "./game/GameProvider";
+// import { GameList } from "./game/GameList";
+
+export const ApplicationViews = () => {
+  return (
+    <>
+      <main
+        style={{
+          margin: "5rem 2rem",
+          backgroundColor: "lightgoldenrodyellow",
+        }}
+      >
+        HERES STUFF
+      </main>
+    </>
+  );
+};
